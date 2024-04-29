dd75da1868a915b88ab3652fded1a87203b8a9e3
辉夜 <rx700@vip.qq.com>
2024-04-29 15:09:24 +0800

    Add files via upload

---
+++ PHPGuard/PHPGuard/GuardClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace PHPGuard
+{
+    public class GuardClass
+    {
+        public static int TimerInterval { get; set; }
+        public static ProcessStartInfo ProcessStartInfo { get; set; } = new ProcessStartInfo();
+        
+        private static Timer Timer { get; set; }
+
+        public static bool StartGuard()
+        {
+            try
+            {
+                Timer = new Timer(Guard, null, 0, TimerInterval);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public static bool StopGuard()
+        {
+            try
+            {
+                Timer?.Change(Timeout.Infinite, Timeout.Infinite);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private static void Guard(object state)
+        {
+            Process[] processes = Process.GetProcessesByName("php-cgi");
+            if (processes.Length == 0)
+            {
+                Console.WriteLine("Process not exist. Restarting now.");
+                Process.Start(ProcessStartInfo);
+            }
+            else
+            {
+                foreach (Process proc in processes)
+                {
+                    if (!proc.Responding)
+                    {
+                        proc.Kill();
+                        Console.WriteLine("Process not responding. Restarting now.");
+                        Process.Start(ProcessStartInfo);
+                    }
+                }
+                Console.WriteLine("Process running stable.");
+            }
+        }
+    }
+}
---
+++ PHPGuard/PHPGuard/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace PHPGuard
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Usage: PHPGuard.exe Interval php-cgi.exe arguments \n e.g.  PHPGuard.exe 10 php-cgi.exe -b 127.0.0.1:9000 -c php.ini");
+                return;
+            }
+
+            if (!int.TryParse(args[0], out int timerInterval))
+            {
+                Console.WriteLine("Invalid timer interval value.");
+                return;
+            }
+
+            string programPath = args[1];
+            if (!programPath.Contains("php-cgi.exe"))
+            {
+                Console.WriteLine("The target program name should be php-cgi.exe.");
+                return;
+            }
+            
+            string arguments = string.Join(" ", args.Skip(2));
+            GuardClass.ProcessStartInfo.Arguments = arguments;
+            GuardClass.ProcessStartInfo.FileName = programPath;
+            GuardClass.ProcessStartInfo.CreateNoWindow = true;
+            GuardClass.ProcessStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            
+            int timerIntervalInMilliseconds = timerInterval * 1000;
+
+            GuardClass.TimerInterval = timerIntervalInMilliseconds; 
+            
+            GuardClass.StartGuard();
+
+            Console.WriteLine("Press any key to stop...");
+            Console.ReadKey();
+        }
+    }
+}
